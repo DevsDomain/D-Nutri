726b14db67488625d80fbbc89193e0ca17d649fd
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-08-16 12:31:47 -0300

    teste

--- front/src/app/index.tsx
+++ front/src/app/index.tsx
@@ -3,7 +3,8 @@ import { View, Text } from "react-native";
 export default function Index() {
     return(
     <View style = {{flex: 1, justifyContent:"center", alignItems: "center"}}>
-        <Text>D-Nutri app</Text> 
+        <Text>D-Nutri app</Text>
+        <Text>Projeto de Aplicação para Nutrição</Text> 
     </View>
     );
 }
