50b7d7d688d1c82327e252c94e5542342316680b
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-09-01 16:00:22 -0300

    Install Fonts pgInitial

---
+++ frontend/src/screens/styles.pgInitial.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  titulo: {
+    color: "darkgreen",
+    fontSize: 30,
+    fontFamily: "Nunito_800ExtraBold",
+  },
+  subtitulo: {
+    color: "darkgreen",
+    fontSize: 20,
+    fontFamily: "Nunito_400Regular",
+  },
+});
