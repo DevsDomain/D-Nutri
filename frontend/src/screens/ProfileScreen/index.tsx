95b09ab11e4a4f16b4d7c08ecb1e0c2a04185dac
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-09-17 22:17:22 -0300

    fix

--- frontend/src/screens/ProfileScreen/index.tsx
+++ frontend/src/screens/ProfileScreen/index.tsx
@@ -1,10 +1,36 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, SafeAreaView, ScrollView, StatusBar } from "react-native";
+import { styles } from "./styles";
+
+
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>Perfil do Usuário</Text>
-    </View>
-  );
-}
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle="default" />
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Configurações</Text>
+        </View>
+  
+        <ScrollView>
+          <ProfilePicture name="User" localImage={localImage} />
+          <SettingsOption
+            label="Editar Perfil"
+            icon="user"
+            onPress={handleEditProfile} />
+          <SettingsOption
+            label="Redefinir Senha"
+            icon="lock"
+            onPress={handlePasswordReset} />
+          <SettingsOption
+            label="Terms & Privacy Policy"
+            icon="file-text"
+            onPress={handleTerms} />
+          <SettingsOption
+            label="Sair da Conta"
+            icon="sign-out"
+            onPress={handleLogout} />
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
