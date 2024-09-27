3c3bfb306221fd8900a99a0d597686dce0b11717
abnercosta97 <abner.costa3@fatec.sp.gov.br>
2024-09-26 23:58:12 -0300

    inserindo process.env e alimento controller

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,12 +3,13 @@ import { Router } from "express";
 import UserRouter from "./userRoutes";
 import DataRouter from "./dataRoutes";
 import FoodRouter from "./alimento";
+require("dotenv").config();
 
 export const routes = Router();
 
 routes.use(
   cors({
-    origin: "http://192.168.0.130:8081", // Permitir requisições do frontend
+    origin: process.env.APP_FRONT_URL || "*", // Permitir requisições do frontend
     methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
     allowedHeaders: ["Content-Type", "Authorization"], // Cabeçalhos permitidos
     credentials: true, // Permitir envio de cookies e headers de autenticação
