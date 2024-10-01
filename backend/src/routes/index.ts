58a742b78e75cf0539cbffcf402fc1ee079148d2
= <moraisdpm@outlook.com>
2024-10-01 16:57:54 -0300

    DN-53 <Subindo CORS para liberar requisicao de todos IPS>

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,9 +2,12 @@ import express from "express";
 import UserRouter from "./userRoutes";
 import DataRouter from "./dataRoutes";
 import DashboardRouter from "./dashboardRoutes";
+import cors from 'cors'
 
 
 const router = express.Router();
+
+router.use(cors)
 router.use(UserRouter);
 router.use(DataRouter);
 router.use(DashboardRouter);
